74a43a29d566aa1f9e98b384e940b3975352a932
manishhsuthar <manishsuthar.dev@gmail.com>
2026-01-09 18:25:07 +0530

    profile setup updated

--- client_old/public/profile-setup.js
+++ client_old/public/profile-setup.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('profile-setup-form');
     let user = null;
 
